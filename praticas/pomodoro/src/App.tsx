3f4013e80533cb37c8ad0873ea529a78f59b636b
Pedrin453 <pedro.francisco@iesb.edu.br>
2026-04-28 21:11:32 -0300

    Feature praticas 60

---
+++ praticas/pomodoro/src/App.tsx
@@ -0,0 +1,13 @@
+import { Home } from './pages/Home';
+import { TaskContextProvider } from './contexts/TaskContext/TaskContextProvider';
+
+import './styles/theme.css';
+import './styles/global.css';
+
+export function App() {
+  return (
+    <TaskContextProvider>
+      <Home />
+    </TaskContextProvider>
+  );
+}
