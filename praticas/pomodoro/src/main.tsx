0c8d58b77eb182fcc24c71326bed9665d553029e
Pedrin453 <pedro.francisco@iesb.edu.br>
2026-05-12 20:29:38 -0300

    atualizando projeto pomodoro com tela de login

--- praticas/pomodoro/src/main.tsx
+++ praticas/pomodoro/src/main.tsx
@@ -1,11 +1,12 @@
-import { createRoot } from 'react-dom/client';
-import { App } from './App.tsx';
-import { StrictMode } from 'react';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { App } from './App'; 
+import { AuthProvider } from './contexts/AuthContext/AuthContext';
 
-createRoot(document.getElementById('root')!).render(
-  <>
-    <StrictMode>
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <AuthProvider>
       <App />
-    </StrictMode>
-  </>,
+    </AuthProvider>
+  </React.StrictMode>
 );
